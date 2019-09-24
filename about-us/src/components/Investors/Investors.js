5dd86a9d0a73ad015458fa8330132b32cc067a2f
Gregory Tang <gtang159@gmail.com>
2019-09-24 12:20:30 -0700

    Added href links to investors images

--- about-us/src/components/Investors/Investors.js
+++ about-us/src/components/Investors/Investors.js
@@ -10,18 +10,22 @@ const Investors = () => {
         <hr className="investors-line"></hr>
         <div className="investors-container">
           <div className="img-container">
-            <img
-              alt="ggv-capital"
-              src={require("../images/Investors/ggv-capital.png")}
-              className="ggv-capital-img"
-            ></img>
+            <a href="https://www.ggvc.com">
+              <img
+                alt="ggv-capital"
+                src={require("../images/Investors/ggv-capital.png")}
+                className="ggv-capital-img"
+              ></img>
+            </a>
           </div>
           <div className="img-container">
-            <img
-              alt="goldman-sachs"
-              src={require("../images/Investors/goldman-sachs.png")}
-              className="goldman-sachs-img"
-            ></img>
+            <a href="https://www.gsipventures.com/content/gsam/us/en/gsip/homepage.html">
+              <img
+                alt="goldman-sachs"
+                src={require("../images/Investors/goldman-sachs.png")}
+                className="goldman-sachs-img"
+              ></img>
+            </a>
           </div>
         </div>
       </div>
