1d2dbcf56bc234d193e9156db9882b95fe8b6d77
Gregory Tang <gtang159@gmail.com>
2019-11-12 15:09:01 -0800

    Altered Redirect section

--- about-us/src/components/Redirect/Redirect.js
+++ about-us/src/components/Redirect/Redirect.js
@@ -1,23 +1,21 @@
 import React from "react";
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Container from "react-bootstrap/Container";
 import "./Redirect.css";
 
 const Redirect = () => {
   return (
-    <Jumbotron fluid className="big-redirect-container">
-      <Container className="small-redirect-container">
+    <div fluid className="big-redirect-container">
+      <div className="small-redirect-container">
         <div className="title-container">
           <div className="title-text">Your ideal match is waiting for you!</div>
         </div>
-        <div className="below-btn-text1">Sign up to view your messages!</div>
+        <div className="below-btn-text1">Sign Up to View Your Matches!</div>
         <div className="btn-container">
           <a href="https://www.wavely.com/?tab=1" className="href-sign-up-btn">
             <button className="sign-up-now-btn">Sign up!</button>
           </a>
         </div>
-      </Container>
-    </Jumbotron>
+      </div>
+    </div>
   );
 };
 
