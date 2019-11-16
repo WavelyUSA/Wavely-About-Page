b12522ded30311cb0f097726a001165c61a4060b
Gregory Tang <gtang159@gmail.com>
2019-11-15 17:22:42 -0800

    Optimization changes for footer

--- about-us/src/App.js
+++ about-us/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "./components/General/NavBar/NavBar";
 import AboutJumbotron from "./components/Jumbotron/Jumbotron";
 import Mission from "./components/Mission/Mission";
-import Ethics from "./components/Ethics/Ethics";
 import Investors from "./components/Investors/Investors";
 import Profiles from "./components/Profiles/Profiles";
 import Redirect from "./components/Redirect/Redirect";
@@ -14,7 +13,6 @@ function App() {
       <NavBar />
       <AboutJumbotron />
       <Mission />
-      {/* <Ethics /> */}
       <Profiles />
       <Investors />
       <Redirect />
