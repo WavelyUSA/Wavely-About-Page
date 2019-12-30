d6ef31fad33ef80c4fc947834e19d64ccd1be3f5
Gregory Tang <gtang159@gmail.com>
2019-12-30 18:47:32 -0500

    Added team video

--- about-us/src/components/Banner/Banner.js
+++ about-us/src/components/Banner/Banner.js
@@ -5,7 +5,7 @@ function Banner() {
   return (
     <div className="banner-container">
       <img
-        src="./jumbotron-image.png"
+        src="./team-photo1.png"
         className="banner-img"
         alt="banner-img"
       ></img>
--- about-us/src/components/Banner2/Banner2.js
+++ about-us/src/components/Banner2/Banner2.js
@@ -1,32 +1,17 @@
 import React from "react";
 import "./Banner2.css";
+import { Player } from "video-react";
 
-class Banner2 extends React.Component {
-  state = {
-    imgURL: "./team-photo1.png"
-  };
-
-  onHover = () => {
-    this.setState({ imgURL: "./team-photo2.png" });
-  };
-
-  onLeave = () => {
-    this.setState({ imgURL: "./team-photo1.png" });
-  };
-
-  render() {
-    return (
-      <div className="banner-container2">
-        <img
-          src={this.state.imgURL}
-          className="banner-img2"
-          alt="banner-img2"
-          onMouseOver={this.onHover}
-          onMouseLeave={this.onLeave}
-        ></img>
-      </div>
-    );
-  }
+function Banner2() {
+  return (
+    <div className="banner-container2">
+      <Player
+        playsInline
+        poster="./video-thumbnail.png"
+        src="wavely-team-video.mp4"
+      />
+    </div>
+  );
 }
 
 export default Banner2;
