f85b4996130910b6cee33d2bdae47ca8c35d85a1
Gregory Tang <gtang159@gmail.com>
2019-12-31 01:29:45 -0500

    converted mp4 to youtube

--- about-us/src/components/Banner2/Banner2.js
+++ about-us/src/components/Banner2/Banner2.js
@@ -5,11 +5,18 @@ import { Player } from "video-react";
 function Banner2() {
   return (
     <div className="banner-container2">
-      <Player
+      {/* <Player
         playsInline
         poster="./video-thumbnail.png"
-        src="./wavely-team-video.mp4"
-      ></Player>
+        src="https://youtu.be/b16sBk9HD1Q"
+      ></Player> */}
+      <iframe
+        src="https://www.youtube.com/embed/b16sBk9HD1Q"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+        title="wavely-team-video"
+      ></iframe>
     </div>
   );
 }
